be6b005e46e12b7e44fec2cd0ae7de596272c252
Will Bumbaugh <weeraphot@bumbaugh.com>
2023-02-28 18:10:46 -0800

    Function practice

---
+++ script.js
@@ -0,0 +1,21 @@
+let productName = "Amazon Smart Thermostat";
+let company = "Brand:Amazon";
+let rating = 4;
+let amazonChoice = true;
+let description =
+  "Amazon smart thermostat - energy star certified, DIY install, Works with Alexa - C - Wire Required";
+let price = 59.99;
+let wireType = "no C-wire Adapter";
+let returnsFree = true;
+let size = "3.56 x 3.56 x 0.84 in";
+let weight = "3.86 oz";
+
+document.write("<h1>" + productName + "</h1>");
+document.write("<h2>" + company + "</h2>");
+document.write("<h3>Rating: " + rating + "</h3>");
+document.write("<h3>Amazon Choice: " + amazonChoice + "</h3>");
+document.write("<p>Description: " + description + "</p>");
+document.write("<p>Price: " + price + "</p>");
+document.write("<p>Wire Type: " + wireType + "</p>");
+document.write("<p>Returns Free: " + returnsFree + "</p>");
+document.write("<p>Size: " + size + "</p>");
